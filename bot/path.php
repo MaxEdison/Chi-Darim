249b2c257e5167fdd87aeaf8f9b28260db092dcc
Alireza <alirezasaadat056@gmail.com>
2024-12-12 15:59:25 +0330

    add: path file

---
+++ bot/path.php
@@ -0,0 +1,20 @@
+<?php
+
+// main file
+$mainPHP = __DIR__;
+
+// config dir
+$configPHP = __DIR__."/config/config.php";
+
+// database dir
+$databaseJSON = __DIR__."/database/database.json";
+$userStateJSON = __DIR__."/database/userState.json";
+
+// functions dir
+$botFunctionsPHP = __DIR__."/functions/botFunctions.php";
+$userFunctionsPHP = __DIR__."/functions/userFunctions.php";
+$requestFunctionsPHP = __DIR__."/functions/requestFunctions.php";
+
+// utils dir
+$keyboardsPHP = __DIR__."/utils/keyboards.php";
+$variablesPHP = __DIR__."/utils/variables.php";
