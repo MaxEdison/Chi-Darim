cf8b8638354fcc52d1de623da02c45c4143e4892
Alireza <alirezasaadat056@gmail.com>
2024-12-17 00:18:52 +0330

    adding setAdmin function

--- bot/functions/userFunctions.php
+++ bot/functions/userFunctions.php
@@ -22,6 +22,7 @@ function setStep($path , $chat_id , $step , $type){
     fclose($file);
 }
 
+
 function getStep($path , $chat_id){
     
     $jsonString = file_get_contents($path);
@@ -35,4 +36,24 @@ function getStep($path , $chat_id){
     
     setStep($path, $chat_id, "start", "new");
     return "start";
+}
+
+
+function setAdmin($path , $chat_id , $stat){
+    
+    $jsonString = file_get_contents($path);
+    $data = json_decode($jsonString , true);
+    
+    for ($i = 0 ; $i < count($data["users"]) ; $i++){
+        if (array_keys($data["users"][$i])[0] == $chat_id){
+            $data["users"][$i]["admin"] = $stat;
+            break;
+        }
+    }
+
+    $jsonString = json_encode($data, JSON_PRETTY_PRINT);
+
+    $file = fopen($path, 'w');
+    fwrite($file, $jsonString);
+    fclose($file);
 }
